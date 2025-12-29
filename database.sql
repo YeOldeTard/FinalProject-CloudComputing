9f8c7ce0a8648c804eb0ca8ca03dc844f4b17a3d
YudhoPatrianto <kydh01123@gmail.com>
2025-12-29 18:53:33 +0700

    database.sql: reworking sql data
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- database.sql
+++ database.sql
@@ -1,30 +1,34 @@
--- Login
+-- Create database
 CREATE DATABASE db_easyfood;
 
--- Use
-USE db_easyfood;
-
--- Login (seller)
+-- table account
 CREATE TABLE account (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    shop_name VARCHAR(300) NOT NULL,
-    shop_image VARCHAR(255) NULL    
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    shop_name VARCHAR(30) NOT NULL,
+    shop_image VARCHAR(50)
 );
 
-
--- Order
-CREATE TABLE customer_order(
+-- table customer order
+CREATE TABLE account (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id VARCHAR(30) NOT NULL,
-    customer VARCHAR(50) NOT NULL,
+    order_id VARCHAR(50) NOT NULL,
+    customer VARCHAR(100) NOT NULL,
     date_order DATE NOT NULL,
-    product_ordered VARCHAR(50) NOT NULL,
+    product_ordered VARCHAR(255) NOT NULL,
     quantity INT NOT NULL,
-    total_price DECIMAL(10,0) NOT NULL,
-    status_order VARCHAR(50) NOT NULL,
-    is_archived tinyint(1) DEFAULT 0,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status_order VARCHAR(30) NOT NULL
 );
 
-INSERT INTO customer_order (order_id, customer, date_order, product_ordered, quantity, total_price, status_order) VALUES ('NQPORWuhblBOZRV', 'Bahlil', '2025-12-25', 'Ethanol 100%', 100, 100000, 'Disiapkan');
+
+-- table product
+CREATE TABLE product (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    product_stock INT NOT NULL,
+    product_price DECIMAL(10, 2) NOT NULL
+);
+
